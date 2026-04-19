70201a0efccffc78b4928ba941e5677696c88049
ss <sasha69.tmk@gmail.com>
2026-04-19 19:35:17 +0300

    Создана модель потока ThreadInfo

---
+++ ProcessManager/Models/ThreadInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics;
+
+namespace ProcessManager.Models
+{
+    public class ThreadInfo
+    {
+        public int Id { get; set; }
+        public ThreadPriorityLevel Priority { get; set; }
+        public System.Diagnostics.ThreadState State { get; set; }
+        public string WaitReason { get; set; }
+    }
+}
